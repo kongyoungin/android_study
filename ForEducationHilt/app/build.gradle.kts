29c7dd1a80551a4aced0b49bf9fc8c4138d3cd68
kongyi <kongyi123@nate.com>
2025-06-22 12:53:04 +0900

    make it use hilt

--- ForEducationHilt/app/build.gradle.kts
+++ ForEducationHilt/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -45,4 +46,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
+
+kapt {
+    correctErrorTypes = true
 }
